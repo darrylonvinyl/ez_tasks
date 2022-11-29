2349e17eb394cb484246cc092f53c9fd0f1fb249
darryl <dawasum2@gmail.com>
2022-11-29 14:32:37 -0700

    Created test module and laid out initial todos

---
+++ ez_tasks.py
@@ -0,0 +1,11 @@
+"""EZ Tasks - Simple task manager"""
+
+#TODO: Define task
+
+#TODO: Add task
+
+#TODO: Edit task
+
+#TODO: Delete task
+
+#TODO: Define taskList
---
+++ test_ez_tasks.py
@@ -0,0 +1,7 @@
+import unittest
+
+class TestTask(unittest.TestCase):
+    pass
+
+class TestTaskList(unittest.TestCase):
+    pass
